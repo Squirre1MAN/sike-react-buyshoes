294bdd826d27568d6997e8b527eef4515e66f398
Squirre1MAN <344900432@qq.com>
2015-10-03 17:27:45 +0800

    To Mission Complete.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 window.onload = function() {
-  console.log("page loaded");
+  console.log("page loaded.");
+  makeCartScrollNicely();
+}
+
+function makeCartScrollNicely() {
+  var cart = document.querySelector(".cart");
+  console.log(cart);
+  Ps.initialize(cart,{
+    wheelSpeed: 2,
+  });
+  console.log("perfect-scrollbar inited.");
 }
