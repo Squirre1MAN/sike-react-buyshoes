59067bd8280e6e0d62e2f7cb17d1fa37a8beac5b
Squirre1MAN <344900432@qq.com>
2015-10-08 14:08:02 +0800

    To implement the Respbonsive layout, t, and make it looks better.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
+
+
+
 window.onload = function() {
   console.log("page loaded.");
   makeCartScrollNicely();
+  toggleRightSideBar();
 }
 
 function makeCartScrollNicely() {
   var cart = document.querySelector(".cart");
-  console.log(cart);
   Ps.initialize(cart,{
     wheelSpeed: 2,
   });
   console.log("perfect-scrollbar inited.");
+}
+
+function toggleRightSideBar() {
+  var $toggle = document.querySelector(".site__right-sidebar-toggle");
+  if ($toggle) {
+    $toggle.addEventListener("click",function() {
+      document.body.classList.toggle("js-show-right-sidebar");
+      console.log("Toggled");
+    });
+  }
+  else {
+    console.log("$toggle element not found");
+  }
 }
